b8326d6aaa5a829a39040d4d3584ce8134b5f69e
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-04-08 09:33:30 +0200

    assigned a new value to array index 0

--- app/src/main/java/za/ac/iie/mysoccerteamapp/MainActivity.kt
+++ app/src/main/java/za/ac/iie/mysoccerteamapp/MainActivity.kt
@@ -1,23 +1,32 @@
 package za.ac.iie.mysoccerteamapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val teams = arrayOf<String>(
+        "Lionel Messi",
+        "Xavi Hernandez",
+        "Andres Iniesta",
+        "Johan Cruyff, ",
+        "Sergio Busquets"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val teams = arrayOf<String>(
-            "Lionel Messi",
-            "Xavi Hernandez",
-            "Andres Iniesta",
-            "Johan Cruyff, ",
-            "Sergio Busquets"
-        )
+        // creating a variable to hold the textView
+        val teamText = findViewById<TextView>(R.id.teamdisplay)
+        // displaying array items in textView
+        // assigned position 0 of team array to a new value
+        teams[0]= "Lionee Messi :)"
+        
+
+
 
 
 
