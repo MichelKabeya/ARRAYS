98aed1889f449dc88dc51c1f7e72cbb4b4e7edc2
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-04-08 09:11:20 +0200

    created an array and added a background

---
+++ app/src/main/java/za/ac/iie/mysoccerteamapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package za.ac.iie.mysoccerteamapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val teams = arrayOf<String>(
+            "Lionel Messi",
+            "Xavi Hernandez",
+            "Andres Iniesta",
+            "Johan Cruyff, ",
+            "Sergio Busquets"
+        )
+
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
